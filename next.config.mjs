529bf4cdeb7b8119afedf80c86f508e7191ee633
retrovisor <andrenfaust@gmail.com>
2024-08-12 11:12:01 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    // Remove the CompressionPlugin temporarily to isolate the issue
-    config.optimization = {
-      splitChunks: {
-        chunks: 'all',
-        maxInitialRequests: Infinity,
-        minSize: 0,
-        maxSize: 250000, // Set a max size for chunks (250KB)
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name(module) {
-              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-              return `npm.${packageName.replace('@', '')}`;
-            },
-            chunks: 'all',
-            enforce: true,
-          },
-        },
-      },
-    };
-
-    return config;
-  },
-
   async rewrites() {
     return [
       {
