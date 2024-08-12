8351e3712575dbf79431b05322a3da62d4c3adad
retrovisor <andrenfaust@gmail.com>
2024-08-12 11:05:23 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,27 @@ import CompressionPlugin from 'compression-webpack-plugin';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
+    // Add aggressive chunk splitting
+    config.optimization = {
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        maxSize: 250000, // Set a max size for chunks (250KB)
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+              return `npm.${packageName.replace('@', '')}`;
+            },
+            chunks: 'all',
+            enforce: true,
+          },
+        },
+      },
+    };
+
     // Add the compression plugin to the webpack config
     config.plugins.push(
       new CompressionPlugin({
