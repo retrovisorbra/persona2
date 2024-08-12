ed87e2e90e348c46c4cb2cb080fb45c9cee0aa08
retrovisor <andrenfaust@gmail.com>
2024-08-12 11:19:09 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack(config) {
+    // Enable tree shaking by ensuring that only the used exports are retained in the final bundle
+    config.optimization.usedExports = true;
+
+    // Enable minification to reduce the size of JavaScript files by removing unnecessary code
+    config.optimization.minimize = true;
+
+    return config;
+  },
+
   async rewrites() {
     return [
       {
